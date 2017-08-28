ba603068f00d75139ceebf922e0e1dc97fa5a5e4
陳鈞廷 <hankchen@f1-god2001.m3.ntu.edu.tw>
2017-08-28 22:07:00 +0800

    Initial Commit

---
+++ openSafariTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  openSafariTest
+//
+//  Created by 陳鈞廷 on 2017/8/28.
+//  Copyright © 2017年 陳鈞廷. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
